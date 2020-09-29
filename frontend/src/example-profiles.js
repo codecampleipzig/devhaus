e186ca2655890ee86fdf89d0eabd0a433e5f11b9
Issah <yussif.issah@gmail.com>
2020-09-29 11:29:13 +0200

    add export default

--- frontend/src/example-profiles.js
+++ frontend/src/example-profiles.js
@@ -1,3 +1,15 @@
 export default [
-    {}
-]
+  {
+    profileName: null,
+    userFirstName: null,
+    userLastName: null,
+    userEmail: null,
+    profilePicture: null,
+    profileLinks: null,
+    userPassword: null,
+    userRole: null,
+    userClass: null,
+    mentorToggel: null,
+    userLanguages: null
+  }
+];
