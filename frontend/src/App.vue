712b8aa5a5c27f2c77a518303f1f51792c1380a5
Matthew Paul O'Brien <obrien.matthew07@gmail.com>
2020-09-29 11:49:55 +0200

    Create Notifications component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,12 @@
 <template>
   <div id="app">
     <router-view />
-    <div class="button-container">
-      <button @click="notify({type: 'info', text:'You have been notified!!!'})">Notify Me!!</button>
-      <button @click="notify({type: 'error', text:'You have been errored!!!'})">Error Me!!!</button>
-    </div>
-    <div class="notification-container">
-      <transition-group name="fade">
-        <Notification
-          v-for="notification in notifications"
-          :key="notification.id"
-          :type="notification.type"
-          :text="notification.text"
-          @remove="$store.commit('REMOVE_NOTIFICATION', notification)"
-        />
-      </transition-group>
-    </div>
-
+   <Notifications />
   </div>
 </template>
 
 <script>
-import Notification from '@/components/Notification.vue';
-import { mapState, mapActions } from 'vuex';
+import Notifications from '@/components/Notifications.vue';
 
 export default {
   data() {
@@ -32,30 +16,21 @@ export default {
   },
   name: 'App',
   components: {
-    Notification,
+    Notifications,
   },
-  computed: mapState(['notifications']),
-  methods: {
-    ...mapActions(['notify']),
-    postError() {
-      this.notify({ type: 'error', text: 'This is an error.' });
-    },
-    removeNotification(notification) {
-      this.$store.commit('REMOVE_NOTIFICATION', notification);
-    },
-  },
+
 };
 </script>
 
 <style>
 .button-container {
   position: fixed;
-  bottom: 0;
+  bottom: 4;
 }
 
 .notification-container {
   position: fixed;
-  top: 0;
+  bottom: 0;
   left: 0;
   width: 100%;
   z-index: 100;
--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -18,6 +18,7 @@ export default {
       required: true,
     },
   },
+
 };
 </script>
 
