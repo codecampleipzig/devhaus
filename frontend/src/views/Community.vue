a4645fc6eec8a2afb7ba9bcf321a4f956c7e000d
Mathias Fidel <fidel.mathias@gmail.com>
2020-09-30 10:23:40 +0200

    Add V-For for profiles

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -2,13 +2,30 @@
   <div>
     <div class="m-40 text-3xl">
       <h1>Community</h1>
+      <div
+        v-for="(profile, index) in profiles"
+        :key="index"
+      >
+        <h3>{{ profile.profileName }}</h3>
+        <div class="image">
+          <img :src="profile.photo">
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import exampleProfiles from '@/example-profiles';
+
 export default {
   name: 'Community',
+  data() {
+    return {
+      profiles: exampleProfiles,
+
+    };
+  },
 };
 </script>
 
