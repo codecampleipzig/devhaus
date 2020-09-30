69926aefa3c5d98a11756953df63b5576476536e
Daniel Cvjeticanin <danicvj@gmail.com>
2020-09-30 16:09:53 +0200

    Added popcorn button

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,15 +6,52 @@
       </h1>
       <p>logged in as: {{ $store.state.user.email }}</p>
     </div>
+    <button
+      id="popcorn"
+      aria-describedby="tooltip"
+    >
+      I'm a dropdown button
+    </button>
+    <div
+      id="tooltip"
+      role="tooltip"
+    >
+      I'm a tooltip
+    </div>
   </div>
 </template>
 
 <script>
+import { createPopper } from '@popperjs/core';
+
 export default {
   name: 'Home',
+  mounted() {
+    const popcorn = document.querySelector('#popcorn');
+    const tooltip = document.querySelector('#tooltip');
+
+    createPopper(popcorn, tooltip, {
+      placement: 'bottom',
+    });
+
+    popcorn.addEventListener('click', () => tooltip.setAttribute('data-show', ''));
+    popcorn.addEventListener('blur', () => tooltip.removeAttribute('data-show'));
+  },
 };
 </script>
 
 <style scoped>
+#tooltip {
+    background-color: #333;
+    color: white;
+    padding: 5px 10px;
+    border-radius: 4px;
+    font-size: 13px;
+    display: none;
+}
+
+#tooltip[data-show] {
+  display: block;
+}
 
 </style>
