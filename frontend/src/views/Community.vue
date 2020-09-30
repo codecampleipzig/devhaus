bbeb810c661096d5b937937e03641e22a8fbcf95
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-09-30 09:40:04 +0200

    Reset Community.vue to develop

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -2,34 +2,13 @@
   <div>
     <div class="m-40 text-3xl">
       <h1>Community</h1>
-      <p>
-        <img
-          class="image"
-          :src="imgUser1"
-        >
-      </p>
     </div>
   </div>
 </template>
 
 <script>
-import imgUser1 from 'https://imgur.com/R4w9K1Y';
-import imgUser2 from 'https://imgur.com/evubUFY';
-import imgUser3 from 'https://imgur.com/AlNofek';
-import imgUser4 from 'https://imgur.com/sMM76pO';
-import imgUser5 from 'https://imgur.com/ONs0NHT';
-import imgUser6 from 'https://imgur.com/YK6Cgfe';
-import imgUser7 from 'https://imgur.com/pYvCuoW';
-import imgUser8 from 'https://imgur.com/LQN1ZKW';
-import imgUser9 from 'https://imgur.com/me9sXOR';
-
 export default {
   name: 'Community',
-  data() {
-    return {
-      Image1: imgUser1,
-    };
-  },
 };
 </script>
 
