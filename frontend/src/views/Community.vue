a406699d08bd0a73adda31a4df55e8f6ed1214cd
Dorian Wilhelm <dorian.wilhelm@outlook.com>
2020-09-30 10:35:21 +0200

    implement Search functionality with fuse.js

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -2,13 +2,47 @@
   <div>
     <div class="m-40 text-3xl">
       <h1>Community</h1>
+      <input
+        v-model="search"
+        type="text"
+        class="bg-gray-400"
+      >
+      <p>{{ result }}</p>
     </div>
   </div>
 </template>
 
 <script>
+import Fuse from 'fuse.js';
+import Profiles from '../example-profiles';
+
 export default {
   name: 'Community',
+  data() {
+    return {
+      fuse: null,
+      profiles: Profiles,
+      result: [],
+      search: '',
+    };
+  },
+  watch: {
+    search() {
+      if (this.search.trim() === '') {
+        this.result = this.list;
+      } else {
+        this.result = this.fuse.search(this.search.trim());
+      }
+    },
+  },
+  mounted() {
+    const options = {
+      includeScore: true,
+      keys: ['profileName', 'firstName', 'lastName', 'email', 'role', 'userlanguages', 'currentProjects.name'],
+    };
+    this.fuse = new Fuse(this.profiles, options);
+    this.result = this.profiles;
+  },
 };
 </script>
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./src/**/*.vue'],
+  purge: [],
   target: 'relaxed',
   prefix: '',
   important: false,
