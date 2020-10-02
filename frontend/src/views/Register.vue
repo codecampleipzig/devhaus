37c3628427da331d43dd01dfa6f2cae2d9fd3917
mnapearson <marratoon@gmail.com>
2020-10-02 10:35:07 +0200

    Add tailwind to the register.vue

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,32 +1,60 @@
 <template>
-  <div>
-    <h3>logged in as {{ $store.state.user.email }}</h3>
-    <form @submit.prevent="submit">
-      <input
-        v-model="userInfo.userName"
-        type="text"
-        placeholder="User Name"
-      >
-      <input
-        v-model="userInfo.firstName"
-        type="text"
-        placeholder="First Name"
-        required
-      >
-      <input
-        v-model="userInfo.lastName"
-        type="text"
-        placeholder="Last Name"
-        required
-      >
-      <input
-        v-model="userInfo.class"
-        type="number"
-        placeholder="Class #"
-        required
-      >
-      <input type="submit">
-    </form>
+  <div class="py-10 flex justify-center">
+    <div
+      class="bg-teal-600 p-16
+  flex flex-col justify-center
+  max-w-screen-sm rounded-lg shadow-lg"
+    >
+      <div class="flex flex-row justify-center">
+        <img
+          class="w-auto rounded-l-full opacity-75"
+          src="https://cdn.wedevs.com/uploads/2019/12/How-to-Customize-Your-WordPress-Login-Screen.png"
+          alt=""
+        >
+
+        <div class="bg-indigo-400 rounded-r-lg p-4 shadow-lg">
+          <h1 class="text-3xl font-bold flex justify-center mt-8">
+            REGISTRATION INFO
+          </h1>
+          <form
+            class="m-12 text-2xl flex flex-col space-y-8 rounded-lg"
+            @submit.prevent="submit"
+          >
+            <input
+              v-model="userInfo.userName"
+              class="p-1 bg-blue-200 max-w-sm rounded"
+              type="text"
+              placeholder="User Name"
+            >
+            <input
+              v-model="userInfo.firstName"
+              class="p-1 bg-blue-200 max-w-sm rounded"
+              type="text"
+              placeholder="First Name"
+              required
+            >
+            <input
+              v-model="userInfo.lastName"
+              class="p-1 bg-blue-200 max-w-sm rounded"
+              type="text"
+              placeholder="Last Name"
+              required
+            >
+            <input
+              v-model="userInfo.class"
+              class="p-1 bg-blue-200 max-w-sm rounded"
+              type="number"
+              placeholder="Class #"
+              required
+            >
+            <input
+              class="p-1 bg-blue-700 max-w-sm rounded-full shadow font-medium"
+              type="submit"
+            >
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
