52f70bee42152cd48ec622767d9af67fd47ce37d
Issah <yussif.issah@gmail.com>
2020-10-02 15:25:40 +0200

    modify p tag

--- frontend/src/views/404.vue
+++ frontend/src/views/404.vue
@@ -4,12 +4,7 @@
 
     <p class="justify-center m-16 text-xl">
       unfortuntely the page you're looking for doesn't exist (anymore)
-    </p>
-    <p class="justify-center m-16 text-xl">
       or
-    </p>
-
-    <p class="justify-center m-16 text-xl">
       there was an error in the link you followed or typed.
     </p>
   </div>
