646f9b0c8a56e2412a96434babb9dae2d25745db
Issah <yussif.issah@gmail.com>
2020-10-02 11:47:30 +0200

    Style 404

--- frontend/src/views/404.vue
+++ frontend/src/views/404.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>(404) Page NotFound</h1>
+  <div class="justify-center m-16 text-5xl font-bold">
+    <h1>404 Page Not Found</h1>
+    <img
+      width="100"
+      src="https://codecampleipzig.de/images/code-camp-leipzig-logo.svg"
+      alt=""
+    >
+    <p class="justify-center m-16 text-3xl">
+      unfortuntely the page you're looking doesn't exist (anymore)
+    </p>
+    <p class="justify-center m-16 text-3xl">
+      or
+    </p>
+    <p class="justify-center m-16 text-3xl">
+      there was an error in the link you followed or typed.
+    </p>
+    <router-link :to="{ name: 'Home'}">
+      go to home page
+    </router-link>
   </div>
 </template>
 
