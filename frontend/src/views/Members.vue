b44825ff47045a1345f6a67e2ebef21a19a5f187
Dorian Wilhelm <dorian.wilhelm@outlook.com>
2020-10-04 16:35:09 +0200

    Highlighting for firstName

--- frontend/src/views/Members.vue
+++ frontend/src/views/Members.vue
@@ -2,6 +2,9 @@
   <div>
     <div class="m-40 text-3xl">
       <h1>Members</h1>
+      <button @click="highlight">
+        Highlight
+      </button>
       <input
         v-model.trim="search"
         type="text"
@@ -20,7 +23,22 @@
           <div
             class="infoContainer"
           >
-            <h3>{{ profile.item.firstName }} {{ profile.item.lastName }}</h3>
+            <div>
+              <span
+                v-for="(segment, index) in getHighlights(profile.item.firstName
+                                                         , profile.matches && profile.matches.find
+                                                           (match => match.key =='firstName'))"
+                :key="segment.text + index + profile.item.userId"
+                class="inline-block m-0"
+                :class="{
+                  'text-blue-500': segment.match
+                }"
+              >
+                {{ segment.text }}
+              </span>
+            </div>
+            <div ref="firstName" />
+            <h3>{{ profile.item.lastName }}</h3>
 
             <div class="role">
               {{ profile.item.class }}
@@ -61,16 +79,75 @@ export default {
       includeScore: true,
       includeMatches: true,
       threshold: 0.5,
-      keys: ['name', 'firstName', 'lastName'],
+      keys: ['firstName'],
     };
     this.fuse = new Fuse(this.profiles, options);
   },
   methods: {
+    getHighlights(string, match) {
+      if (!match) {
+        return [{
+          text: string,
+          match: false,
+        }];
+      }
+      const segments = [];
+      let index = 0;
+      let currentIndices = 0;
+      const { indices } = match;
+      while (index < string.length) {
+        const nextMatch = indices[currentIndices];
+        if (!nextMatch) {
+          segments.push({
+            text: string.slice(index),
+            match: false,
+          });
+          break;
+        }
+        const [matchStart, matchEnd] = nextMatch;
+
+        if (nextMatch[0] > index) {
+          segments.push({
+            text: string.slice(index, matchStart),
+            match: false,
+          });
+          index = matchStart;
+        } else {
+          segments.push({
+            text: string.slice(matchStart, matchEnd + 1),
+            match: true,
+          });
+          currentIndices += 1;
+          index = matchEnd + 1;
+        }
+      }
+
+      return segments;
+    },
     getSearchCollection() {
       return this.profiles.map((profile) => ({
         ...profile,
         name: `${profile.firstName} ${profile.lastName}`,
       }));
+    },
+    highlight() {
+      const searchResults = this.results;
+      const firstMatchedIndex = searchResults[0].matches[0].indices[0][0];
+      const lastMatchedInedex = searchResults[0].matches[0].indices[0][1];
+
+      // const splitFirstName = searchResults[0].item.firstName.split('');
+
+      // for (let i = 0; i < splitFirstName.length; i += 1) {
+      //   if (i >= firstMatchedIndex && i <= lastMatchedInedex) {
+      //     const span = document.createElement('span');
+      //     span.innerHTML = i;
+      //     this.$refs.firstName.appendChild(span);
+      //   }
+      // }
+
+      console.log(searchResults);
+      console.log(firstMatchedIndex);
+      console.log(lastMatchedInedex);
     },
   },
 };
