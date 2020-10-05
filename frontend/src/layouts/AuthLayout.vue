53381a92b49319f416203a2a0db1c52c7d7fa45b
mnapearson <marratoon@gmail.com>
2020-10-05 15:55:52 +0200

    Fix justify-center on mobile view

--- frontend/src/layouts/AuthLayout.vue
+++ frontend/src/layouts/AuthLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-evenly min-h-screen bg-gray-100">
+  <div class="flex items-center md:justify-evenly justify-center min-h-screen bg-gray-100">
     <section class="text-5xl font-semibold leading-none md:uppercase flex md:flex-col">
       <div
         class="md:bg-teal-900 md:h-64 md:w-64 text-white items-end
